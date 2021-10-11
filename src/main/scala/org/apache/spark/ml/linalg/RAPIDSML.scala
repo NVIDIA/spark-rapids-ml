8761c7a4b0aa46ffeb18156b1ca02aa839b44749
Allen Xu <allxu@nvidia.com>
2021-10-11 20:23:58 +0800

    Update javadoc

--- src/main/scala/org/apache/spark/ml/linalg/RAPIDSML.scala
+++ src/main/scala/org/apache/spark/ml/linalg/RAPIDSML.scala
@@ -51,16 +51,36 @@ private[spark] object RAPIDSML extends Serializable {
   }
 
   /**
-   * C := B.transpose * B
+   * Wrapper of Cublas gemm function. More details: https://docs.nvidia.com/cuda/cublas/index.html#cublas-lt-t-gt-gemm
    *
-   * @param B the matrix B that will be left multiplied by its transpose. Size of m x n.
-   * @param C the resulting matrix C. Size of n x n.
+   * @param transa int type representing the operation op(A) that is non- or (conj.) transpose.
+   * @param transb int type representing the operation op(B) that is non- or (conj.) transpose.
+   * @param m number of rows of matrix op(A) and C.
+   * @param n number of columns of matrix op(B) and C.
+   * @param k number of columns of op(A) and rows of op(B).
+   * @param alpha scalar used for multiplication.
+   * @param A array of dimensions lda x k with lda>=max(1,m) if transa == CUBLAS_OP_N and lda x m with lda>=max(1,k) otherwise.
+   * @param lda leading dimension of two-dimensional array used to store the matrix A.
+   * @param B array of dimension ldb x n with ldb>=max(1,k) if transb == CUBLAS_OP_N and ldb x k with ldb>=max(1,n) otherwise.
+   * @param ldb leading dimension of two-dimensional array used to store matrix B.
+   * @param beta scalar used for multiplication. If beta==0, C does not have to be a valid input.
+   * @param C array of dimensions ldc x n with ldc>=max(1,m).
+   * @param ldc leading dimension of a two-dimensional array used to store the matrix C.
+   * @param deviceID the device that will run the computation
    */
   def gemm(transa: Int, transb: Int, m: Int, n: Int, k: Int, alpha: Double, A: DenseMatrix, lda: Int,
            B: DenseMatrix, ldb: Int,beta: Double,C: DenseMatrix, ldc: Int, deviceID: Int): Unit = {
     jniRAPIDSML.dgemm(transa, transb, m, n, k, alpha, A.values, lda, B.values, ldb, beta, C.values, ldc, deviceID)
   }
 
+  /**
+   *
+   * @param m size of sqiare matrix A
+   * @param A raw matrix to be decomposed, size of m * m
+   * @param U left matrix after decomposition
+   * @param S middle vector after decomposition
+   * @param deviceID the device that will run the computation
+   */
   def calSVD(m: Int, A: DenseMatrix, U: DenseMatrix, S: DenseMatrix, deviceID: Int): Unit = {
     require(m == A.numRows, s"The rows of A don't match required dimension")
     require(m == A.numCols, s"The cols of A don't match required dimension")
