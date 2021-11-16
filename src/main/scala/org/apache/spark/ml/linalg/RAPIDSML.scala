48298d030b69c825b4ffac203b5098ea01ba8f0d
Allen Xu <wjxiz1992@gmail.com>
2021-11-16 14:54:52 +0800

    Use spark-rapids plugin to enable columnar process for PCA transform (#19)
    * test
    
    Signed-off-by: Allen Xu <allxu@nvidia.com>
    
    * update
    
    Signed-off-by: Allen Xu <allxu@nvidia.com>
    
    * temp save
    
    Signed-off-by: Allen Xu <allxu@nvidia.com>
    
    * close cv
    
    Signed-off-by: Allen Xu <allxu@nvidia.com>
    
    * UDF GPU transform with Array input not vector
    
    * resolve NPE
    
    Signed-off-by: Allen Xu <allxu@nvidia.com>
    
    * native success
    
    Signed-off-by: Allen Xu <allxu@nvidia.com>
    
    * Add native transpose
    
    Signed-off-by: Allen Xu <allxu@nvidia.com>
    
    * Add cudfUtils
    
    Signed-off-by: Allen Xu <allxu@nvidia.com>
    
    * transform works
    
    Signed-off-by: Allen Xu <allxu@nvidia.com>
    
    * refine
    
    Signed-off-by: Allen Xu <allxu@nvidia.com>
    
    * clean
    
    Signed-off-by: Allen Xu <allxu@nvidia.com>
    
    * use fromRmm
    
    Signed-off-by: Allen Xu <allxu@nvidia.com>
    
    * use fromRmm
    
    Signed-off-by: Allen Xu <allxu@nvidia.com>
    
    * clean
    
    Signed-off-by: Allen Xu <allxu@nvidia.com>
    
    * Update cmake to use libcudf
    
    Signed-off-by: Allen Xu <allxu@nvidia.com>
    
    * build target column in native
    
    Signed-off-by: Allen Xu <allxu@nvidia.com>
    
    * run successfully
    
    * refine
    
    Signed-off-by: Allen Xu <allxu@nvidia.com>
    
    * add try catch
    
    Signed-off-by: Allen Xu <allxu@nvidia.com>
    
    * clean
    
    Signed-off-by: Allen Xu <allxu@nvidia.com>
    
    * clean
    
    Signed-off-by: Allen Xu <allxu@nvidia.com>
    
    * modify gemm parameters
    
    Signed-off-by: Allen Xu <allxu@nvidia.com>
    
    * add doc
    
    Signed-off-by: Allen Xu <allxu@nvidia.com>
    
    * Resolve some comments
    
    Signed-off-by: Allen Xu <allxu@nvidia.com>
    
    * Update API according to comments
    
    - jni_utils.hpp from cuDF will cause compile error
    
    * polish
    
    Signed-off-by: Allen Xu <allxu@nvidia.com>
    
    * tmp save for debug
    
    Signed-off-by: Allen Xu <allxu@nvidia.com>
    
    * fix some errors
    
    Signed-off-by: Allen Xu <allxu@nvidia.com>
    
    * rename
    
    * fix error
    
    Signed-off-by: Allen Xu <allxu@nvidia.com>
    
    * rename file
    
    Signed-off-by: Allen Xu <allxu@nvidia.com>
    
    * Add header
    
    * optimize
    
    * Update raft header file name
    
    * resolve comments
    
    * idea save
    
    * rename
    
    Signed-off-by: Allen Xu <allxu@nvidia.com>
    
    Co-authored-by: Jason Lowe <jlowe@nvidia.com>

--- src/main/scala/org/apache/spark/ml/linalg/RAPIDSML.scala
+++ src/main/scala/org/apache/spark/ml/linalg/RAPIDSML.scala
@@ -16,7 +16,9 @@
 
 package org.apache.spark.ml.linalg
 
+import ai.rapids.cudf.ColumnView
 import com.nvidia.spark.ml.linalg.JniRAPIDSML
+import org.apache.spark.ml.linalg.RAPIDSML.CublasOperationT.CublasOperationT
 
 /**
  * BLAS routines for MLlib's vectors and matrices.
@@ -42,15 +44,6 @@ private[spark] object RAPIDSML extends Serializable {
   }
 
   /**
-   * Adds alpha * v * v.t to a matrix in-place. This is the same as BLAS's ?SPR.
-   *
-   * @param U the upper triangular part of the matrix packed in an array (column major)
-   */
-  def spr(v: DenseVector, U: Array[Double]): Unit = {
-    jniRAPIDSML.dspr(v.size, v.values, U)
-  }
-
-  /**
    * Wrapper of Cublas gemm function. More details: https://docs.nvidia.com/cuda/cublas/index.html#cublas-lt-t-gt-gemm
    *
    * @param transa int type representing the operation op(A) that is non- or (conj.) transpose.
@@ -71,6 +64,52 @@ private[spark] object RAPIDSML extends Serializable {
   def gemm(transa: Int, transb: Int, m: Int, n: Int, k: Int, alpha: Double, A: DenseMatrix, lda: Int,
            B: DenseMatrix, ldb: Int,beta: Double,C: DenseMatrix, ldc: Int, deviceID: Int): Unit = {
     jniRAPIDSML.dgemm(transa, transb, m, n, k, alpha, A.values, lda, B.values, ldb, beta, C.values, ldc, deviceID)
+  }
+
+  /**
+   * Wrapper of cuBLAS GEMM routine to do matrix computation on GPU.
+   * Most parameters are the same as: https://docs.nvidia.com/cuda/cublas/index.html#cublas-lt-t-gt-gemm
+   * but modified B and removed C for integration compatibility.
+   * B: ColumnView that is holding the matrix data to be transformed on device.
+   * C: as the computation output is removed from parameter list and becomes a return value of Long type that represents
+   * the handle of a LIST type `cudf::column_view *` which is holding the computation output.
+   *
+   * @param transa CublasOperationT enum value representing the operation op(A) that is non- or (conj.) transpose.
+   * @param transb CublasOperationT enum value representing the operation op(B) that is non- or (conj.) transpose.
+   * @param m number of rows of matrix op(A) and C.
+   * @param n number of columns of matrix op(B) and C.
+   * @param k number of columns of op(A) and rows of op(B).
+   * @param alpha scalar used for multiplication.
+   * @param A array of dimensions lda x k with lda>=max(1,m) if transa == CUBLAS_OP_N and lda x m with lda>=max(1,k) otherwise.
+   * @param lda leading dimension of two-dimensional array used to store the matrix A.
+   * @param B ColumnView that holds the device matrix data
+   *          (Array of dimension ldb x n with ldb>=max(1,k) if transb == CUBLAS_OP_N and ldb x k with ldb>=max(1,n) otherwise.)
+   * @param ldb leading dimension of two-dimensional array used to store matrix B.
+   * @param beta scalar used for multiplication. If beta==0, C does not have to be a valid input.
+   * @param ldc leading dimension of a two-dimensional array used to store the matrix C.
+   * @param deviceID the device that will run the computation
+   * @return value of Long type that represents the handle of a LIST type `cudf::column_view *` which is holding the computation output.
+   *         It can be used to construct ColumnVector.
+   */
+  def gemm(transa: CublasOperationT, transb: CublasOperationT, m: Int, n: Int, k: Int, alpha: Double, A: Array[Double],
+           lda: Int, B: ColumnView, ldb: Int,beta: Double, ldc: Int, deviceID: Int): Long = {
+    jniRAPIDSML.dgemmWithColumnViewPtr(transa.id, transb.id, m, n, k, alpha, A, lda, B.getNativeView, ldb, beta, ldc,
+      deviceID)
+  }
+
+  /**
+   * Wrapper for cuBLAS GEMM routine used for PCA transform but with fewer input parameters.
+   * Detailed parameters can be inferred from input DenseMatrix.
+   *
+   * @param A input DenseMatrix, Here it's the principal components model from PCA training.
+   * @param B ColumnView that holds B matrix data on device. Here it's raw data to be transformed by PCA.
+   * @param deviceID the device that will run the computation
+   * @return value of Long type that represents the handle of a LIST type `cudf::column_view *` which is holding the computation output.
+   *         It can be used to construct ColumnVector.
+   */
+  def gemm(A: DenseMatrix, B: ColumnView, deviceID: Int): Long = {
+    gemm(RAPIDSML.CublasOperationT.CUBLAS_OP_T, RAPIDSML.CublasOperationT.CUBLAS_OP_N, A.numCols, B.getRowCount.toInt,
+      A.numRows, 1.0, A.values, A.numRows, B, A.numRows, 0.0, A.numCols, deviceID)
   }
 
   /**
