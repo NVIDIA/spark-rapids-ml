6da6f7fdf9fd1add7731291584105bd5e84adc6c
Bobby Wang <wbo4958@gmail.com>
2022-10-13 18:10:50 -0700

    Add the common  _fit flow

--- sparkcuml/core.py
+++ sparkcuml/core.py
@@ -13,10 +13,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from abc import abstractmethod
+from typing import Any, Union, Iterator
+
+import cudf
+import pandas as pd
 from pyspark.ml import Estimator
 from pyspark.ml.param import Param, Params, TypeConverters
 from pyspark.ml.param.shared import HasInputCols
 from pyspark.sql import DataFrame
+from pyspark.sql.types import StructType, Row
 
 
 class _CumlEstimatorParams(HasInputCols):
@@ -47,5 +53,72 @@ class _CumlEstimator(Estimator, _CumlEstimatorParams):
             num_workers=1,
         )
 
+    @abstractmethod
+    def _fit_internal(self, df: list[cudf.DataFrame], **kwargs) -> dict[str, Any]:
+        """
+        Subclass must implement its own logic to fit a model to the input dataset.
+        Please note that, this function is called on the executor side.
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def _out_schema(self) -> Union[StructType, str]:
+        """
+        The output schema of the estimator, which will be used to
+        construct the returning pandas dataframe
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def _create_pyspark_model(self, result: Row):
+        """
+        Create the model according to the collected Row
+        """
+        raise NotImplementedError()
+
+    def _repartition_dataset(self, dataset: DataFrame) -> DataFrame:
+        """
+        Repartition the dataset to the desired number of workers.
+        """
+        return dataset.repartition(self.getOrDefault(self.num_workers))
+
     def _fit(self, dataset: DataFrame):
-        pass
+        input_col_names = self.getInputCols()
+        dataset = dataset.select(*input_col_names)
+        dataset = self._repartition_dataset(dataset)
+        params = {}
+
+        def _cuml_fit(pdf_iter: Iterator[pd.DataFrame]):
+            from pyspark import BarrierTaskContext
+            context = BarrierTaskContext.get()
+            context.barrier()
+
+            inputs = []
+
+            if input_col_names:
+                for pdf in pdf_iter:
+                    inputs.append(cudf.DataFrame(pdf[input_col_names]))
+            else:
+                # TODO do we need to support features (vector or array type) ??
+                for pdf in pdf_iter:
+                    flatten = pdf.apply(lambda x: x['features'], axis=1, result_type='expand')
+                    gdf = cudf.from_pandas(flatten)
+                    inputs.append(gdf)
+
+            result = self._fit_internal(inputs, **params)
+
+            context.barrier()
+            if context.partitionId() == 0:
+                yield pd.DataFrame(data=result)
+
+        ret = (
+            dataset.mapInPandas(
+                _cuml_fit,
+                schema=self._out_schema(),
+            )
+            .rdd.barrier()
+            .mapPartitions(lambda x: x)
+            .collect()[0]
+        )
+
+        return self._copyValues(self._create_pyspark_model(ret))
