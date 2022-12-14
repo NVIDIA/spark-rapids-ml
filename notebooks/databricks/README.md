1a623367c9da0ea3fd6aed704679427edb7741ae
Erik Ordentlich <eordentlich@gmail.com>
2022-12-14 13:19:49 -0800

    update databricks init script

--- notebooks/databricks/README.md
+++ notebooks/databricks/README.md
@@ -1,6 +1,6 @@
 To run notebooks on Databricks do the following:
 - If you don't already have it, install [databricks cli](https://learn.microsoft.com/en-us/azure/databricks/dev-tools/cli/) and create and save an [access token](https://docs.databricks.com/dev-tools/api/latest/authentication.html) to your workspace using the workspace UI. 
 - Create a zip file of `sparkcuml` directory via `zip -r sparkcuml.zip sparkcuml` command at the top level of the repo and copy to a location in dbfs using the databricks cli command `databricks fs cp sparkcuml.zip <dbfs:/dbfs location>` 
-- Edit the file [init-pip-11-0.sh](init-pip-11-0.sh) to set the `SPARKCUML_ZIP` variable to the dbfs location used above and upload the resulting modifed file to dbfs using the 
+- Edit the file [init-pip-11.8.sh](init-pip-11.8.sh) to set the `SPARKCUML_ZIP` variable to the dbfs location used above and upload the resulting modifed file to dbfs using the 
 - Create a cluster using Databricks 10.4LTS runtime (11.3 is not yet compatible with spark-rapids) using at least 2 gpu based workers and add the init file dbfs location used above to the spark-config section of the cluster.
 - Select your workspace and upload desired notebook via `Import` in the drop down menu for your workspace.
---
+++ notebooks/databricks/init-pip-cuda-11.8.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+SPARKCUML_ZIP=
+RAPIDS_VERSION=22.12.0
+SPARK_RAPIDS_VERSION=22.10.0
+
+curl -L https://repo1.maven.org/maven2/com/nvidia/rapids-4-spark_2.12/${SPARK_RAPIDS_VERSION}/rapids-4-spark_2.12-${SPARK_RAPIDS_VERSION}.jar -o /databricks/jars/rapids-4-spark_2.12-${SPARK_RAPIDS_VERSION}.jar 
+
+# install cudatoolkit 11.8 via runfile approach
+wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda_11.8.0_520.61.05_linux.run
+sh cuda_11.8.0_520.61.05_linux.run --silent --toolkit
+
+# install forward compatibility package due to old driver
+distro=ubuntu2004
+arch=x86_64
+apt-key del 7fa2af80
+wget https://developer.download.nvidia.com/compute/cuda/repos/$distro/$arch/cuda-keyring_1.0-1_all.deb
+dpkg -i cuda-keyring_1.0-1_all.deb
+apt-get update
+apt-get install -y cuda-compat-11-8
+
+
+# reset symlink and update library loading paths
+# **** set LD_LIBRARY_PATH as below in env var section of cluster config in DB cluster UI ****
+rm /usr/local/cuda
+ln -s /usr/local/cuda-11.8 /usr/local/cuda
+export LD_LIBRARY_PATH=/usr/local/cuda/compat:/usr/local/cuda/lib64
+ldconfig
+
+# upgrade pip
+/databricks/python/bin/pip install --upgrade pip
+
+# install cudf
+/databricks/python/bin/pip install cudf-cu11==${RAPIDS_VERSION} cuml-cu11==${RAPIDS_VERSION} --extra-index-url=https://pypi.ngc.nvidia.com
+
+# install spark-cuml
+unzip ${SPARKCUML_ZIP} -d /databricks/python3/lib/python3.8/site-packages
+
