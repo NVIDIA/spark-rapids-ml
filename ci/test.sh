c8ddb89fcc6ad2008db001c77590710838bf9691
Peixin <pxli@nyu.edu>
2023-03-17 10:19:57 +0800

    Init CI environment and test script (#157)
    * init CI environment and test scripts
    
    Signed-off-by: Peixin Li <pxli@nyu.edu>
    
    * use tooling image as default agent
    
    Signed-off-by: Peixin Li <pxli@nyu.edu>
    
    * rename CI image name
    
    ---------
    
    Signed-off-by: Peixin Li <pxli@nyu.edu>

---
+++ ci/test.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Copyright (c) 2023, NVIDIA CORPORATION. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -ex
+
+type="$1"
+case $type in
+  "pre-merge" | "")
+    ut_args=""
+    ;;
+  "nightly")
+    ut_args="--runslow"
+    ;;
+  *)
+    echo "Unknown test type: $type"; exit 1;;
+esac
+bench_args=""
+
+# environment
+nvidia-smi
+which python
+pip install --no-cache -r requirements_dev.txt && pip install --no-cache -e .
+
+# unit tests
+./run_test.sh $ut_args
+
+# benchmark
+./run_benchmark.sh $bench_args
