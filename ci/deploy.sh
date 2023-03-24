6f23e3fdf6d22a762e9210e47c885dd3b42a60a1
Lee Yang <leewyang@gmail.com>
2023-03-23 17:45:56 -0700

    add deploy.sh script (#179)
    add ci/deploy.sh script
    
    Signed-off-by: Lee Yang <leewyang@gmail.com>

---
+++ ci/deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# get version tag
+TAG=$(git describe --tag)
+if [[ $? != 0 ]]; then
+    echo "Can only deploy from a version tag."
+    exit 1
+fi
+
+# exit on first fail
+set -e
+
+# build and publish pip package
+pushd python
+pip install -r requirements_dev.txt && pip install -e .
+python -m build
+# TODO: python -m twine upload --repository pypi dist/*
+popd
+
+# build and publish docs
+pushd docs
+make html
+git worktree add --track -b gh-pages _site origin/gh-pages
+cp -r build/html/* _site/api/python
+cp -r site/* _site
+pushd _site
+git add --all
+git commit -m "${TAG}"
+git push origin gh-pages
+popd #_site
+git worktree remove _site
+popd
+
