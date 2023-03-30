95ba0c967609b7a0ae4059ccb7de05c88a7ccc35
YanxuanLiu <yanxuanl@nvidia.com>
2023-03-30 13:06:08 +0800

    add header
    Signed-off-by: YanxuanLiu <yanxuanl@nvidia.com>

--- ci/docs.sh
+++ ci/docs.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+#
+# Copyright (c) 2023, NVIDIA CORPORATION. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 # get version tag
 TAG=$(git describe --tag)
@@ -7,8 +22,7 @@ if [[ $? != 0 ]]; then
     exit 1
 fi
 
-# exit on first fail
-set -e
+set -ex
 
 # build and publish docs
 pushd docs
