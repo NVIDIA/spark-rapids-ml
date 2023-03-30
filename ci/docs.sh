91f5d68bb961aa72adf248e8d83904362f082557
YanxuanLiu <104543031+YanxuanLiu@users.noreply.github.com>
2023-03-30 13:18:35 +0800

    Split deploy script [skip ci] (#187)
    split deploy script
    
    Signed-off-by: YanxuanLiu <yanxuanl@nvidia.com>

--- ci/docs.sh
+++ ci/docs.sh
@@ -15,10 +15,25 @@
 # limitations under the License.
 #
 
+# get version tag
+TAG=$(git describe --tag)
+if [[ $? != 0 ]]; then
+    echo "Can only deploy from a version tag."
+    exit 1
+fi
+
 set -ex
 
-# build whl package
-pushd python
-pip install -r requirements_dev.txt && pip install -e .
-python -m build
+# build and publish docs
+pushd docs
+make html
+git worktree add --track -b gh-pages _site origin/gh-pages
+cp -r build/html/* _site/api/python
+cp -r site/* _site
+pushd _site
+git add --all
+git commit -m "${TAG}"
+git push origin gh-pages
+popd #_site
+git worktree remove _site
 popd
