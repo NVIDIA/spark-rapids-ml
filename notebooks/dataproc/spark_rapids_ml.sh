62451d5d4ae98901e8494c2e1e1281906734a772
Lee Yang <leewyang@gmail.com>
2023-04-10 16:29:04 -0700

    patch ucx
    Signed-off-by: Lee Yang <leewyang@gmail.com>

--- notebooks/dataproc/spark_rapids_ml.sh
+++ notebooks/dataproc/spark_rapids_ml.sh
@@ -15,5 +15,8 @@ pylibraft-cu11==${RAPIDS_VERSION} \
 rmm-cu11==${RAPIDS_VERSION} \
 --extra-index-url=https://pypi.nvidia.com
 
+# rapids pip package patch: link ucx libraries in ucx-py to default location searched by raft_dask
+ln -s /opt/conda/miniconda3/lib/python3.8/site-packages/ucx_py_cu11.libs/ucx /usr/lib/ucx
+
 # install spark-rapids-ml
 pip install spark-rapids-ml
