04c16a07e7e7bec09eb2a5e69d182a7156f8393e
Jinfeng Li <lijinf8@gmail.com>
2023-04-21 18:13:31 -0700

    Support deployment to AWS EMR (#242)
    * support AWS EMR deployment
    
    Signed-off-by: Jinfeng <jinfengl@nvidia.com>
    
    * Got all notebooks running
    
    Signed-off-by: Jinfeng <jinfengl@nvidia.com>
    
    * revise kmeans notebook to avoid using GPU on driver
    
    Signed-off-by: Jinfeng <jinfengl@nvidia.com>
    
    * refine README
    
    Signed-off-by: Jinfeng <jinfengl@nvidia.com>
    
    * got all notebook passed
    
    Signed-off-by: Jinfeng <jinfengl@nvidia.com>
    
    * revise PR
    
    Signed-off-by: Jinfeng <jinfengl@nvidia.com>
    
    * set to all core nodes and revise json format of the configuration file
    
    Signed-off-by: Jinfeng <jinfengl@nvidia.com>
    
    ---------
    
    Signed-off-by: Jinfeng <jinfengl@nvidia.com>
    Co-authored-by: Jinfeng <jinfengl@nvidia.com>

---
+++ notebooks/aws-emr/init-bootstrap-action.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -ex
+ 
+sudo chmod a+rwx -R /sys/fs/cgroup/cpu,cpuacct
+sudo chmod a+rwx -R /sys/fs/cgroup/devices
+
+sudo yum install -y gcc openssl-devel bzip2-devel libffi-devel tar gzip wget make mysql-devel
+sudo bash -c "wget https://www.python.org/ftp/python/3.9.9/Python-3.9.9.tgz && tar xzf Python-3.9.9.tgz && cd Python-3.9.9 && ./configure --enable-optimizations && make altinstall"
+
+RAPIDS_VERSION=23.2.0
+
+# install scikit-learn 
+sudo /usr/local/bin/pip3.9 install scikit-learn
+
+# install cudf and cuml
+sudo /usr/local/bin/pip3.9 install --no-cache-dir cudf-cu11==${RAPIDS_VERSION} \
+    cuml-cu11==${RAPIDS_VERSION} \
+    pylibraft-cu11==${RAPIDS_VERSION} \
+    rmm-cu11==${RAPIDS_VERSION} \
+    --extra-index-url=https://pypi.nvidia.com
+
