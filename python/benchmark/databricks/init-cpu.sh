816aa41392fa9ea718b7512b66c5a61f6490b39c
Erik Ordentlich <eordentlich@gmail.com>
2023-06-23 14:38:26 -0700

    update python location for cpu init
    Signed-off-by: Erik Ordentlich <eordentlich@gmail.com>

--- python/benchmark/databricks/init-cpu.sh
+++ python/benchmark/databricks/init-cpu.sh
@@ -4,6 +4,7 @@ SPARK_RAPIDS_ML_ZIP=/dbfs/path/to/spark-rapids-ml.zip
 BENCHMARK_ZIP=/dbfs/path/to/benchmark.zip
 
 # install spark-rapids-ml
-unzip ${SPARK_RAPIDS_ML_ZIP} -d /databricks/python3/lib/python3.8/site-packages
-unzip ${BENCHMARK_ZIP} -d /databricks/python3/lib/python3.8/site-packages
+python_ver=`python --version | grep -oP '3\.[0-9]+'`
+unzip ${SPARK_RAPIDS_ML_ZIP} -d /databricks/python3/lib/python${python_ver}/site-packages
+unzip ${BENCHMARK_ZIP} -d /databricks/python3/lib/python${python_ver}/site-packages
 
