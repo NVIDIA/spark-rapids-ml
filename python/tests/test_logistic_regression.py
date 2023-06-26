6a3ad99c2c035b3e4712d10add9e337fb9ab71d0
Jinfeng <jinfengl@nvidia.com>
2023-06-26 11:33:37 -0700

    clean code
    Signed-off-by: Jinfeng <jinfengl@nvidia.com>

--- python/tests/test_logistic_regression.py
+++ python/tests/test_logistic_regression.py
@@ -58,10 +58,7 @@ def test_toy_example(gpu_number: int) -> None:
         assert lr_model.intercept_ == pytest.approx([-2.2614916e-08], abs=1e-6)
         assert lr_model.n_cols == 2
         assert lr_model.dtype == "float32"
-        
-    #from cuml import LogisticRegression as CuLogisticRegression
-    #cuml_lr = CuLogisticRegression()
-    #cuml_lr.fit(X, y)
+
 
 #@pytest.mark.parametrize("data_shape", [(2000, 8)], ids=idfn)
 #@pytest.mark.parametrize("data_type", cuml_supported_data_types)
@@ -95,14 +92,6 @@ def test_classifier(
         n_repeated=0,
     )
 
-    #rf_params: Dict[str, Any] = {
-    #    "n_estimators": 100,
-    #    "n_bins": 128,
-    #    "max_depth": 16,
-    #    "bootstrap": False,
-    #    "max_features": 1.0,
-    #}
-
     from cuml import LogisticRegression as cuLR
     cu_lr = cuLR()
     cu_lr.fit(X_train, y_train)
@@ -121,8 +110,6 @@ def test_classifier(
         spark_lr.setLabelCol(label_col)
         spark_lr_model: LogisticRegressionModel = spark_lr.fit(train_df)
 
-        #print(f"spark_lr_model.coef_: {spark_lr_model.coef_}")
-        #print(f"spark_lr_model.intercept_: {spark_lr_model.intercept_}")
         assert len(spark_lr_model.coef_) == len(cu_lr.coef_)
         for i in range(len(spark_lr_model.coef_)):
             assert spark_lr_model.coef_[i] == pytest.approx(cu_lr.coef_[i], tolerance)
@@ -132,46 +119,3 @@ def test_classifier(
         assert spark_lr_model.n_cols == cu_lr.n_cols
 
         assert spark_lr_model.dtype == cu_lr.dtype
-
-        #test_df, _, _ = create_pyspark_dataframe(
-        #    spark, feature_type, data_type, X_test, y_test
-        #)
-
-        #result = spark_lr_model.transform(test_df).collect()
-        #pred_result = [row.prediction for row in result]
-
-        #if feature_type == feature_types.vector:
-        #    # no need to compare all feature type.
-        #    spark_cpu_result = spark_rf_model.cpu().transform(test_df).collect()
-        #    spark_cpu_pred_result = [row.prediction for row in spark_cpu_result]
-        #    assert array_equal(spark_cpu_pred_result, pred_result)
-
-        #spark_acc = accuracy_score(y_test, np.array(pred_result))
-
-        ## Since vector type will force to convert to array<double>
-        ## which may cause precision issue for random forest.
-        #if num_workers == 1 and not (
-        #    data_type == np.float32 and feature_type == feature_types.vector
-        #):
-        #    assert cu_acc == spark_acc
-
-        #    pred_proba_result = [row.probability for row in result]
-        #    np.testing.assert_allclose(pred_proba_result, cu_preds_proba, rtol=1e-3)
-        #else:
-        #    assert cu_acc - spark_acc < 0.07
-
-        ## for multi-class classification evaluation
-        #if n_classes > 2:
-        #    from pyspark.ml.evaluation import MulticlassClassificationEvaluator
-
-        #    evaluator = MulticlassClassificationEvaluator(
-        #        predictionCol=spark_rf_model.getPredictionCol(),
-        #        labelCol=spark_rf_model.getLabelCol(),
-        #    )
-
-        #    spark_cuml_f1_score = spark_rf_model._transformEvaluate(test_df, evaluator)
-
-        #    transformed_df = spark_rf_model.transform(test_df)
-        #    pyspark_f1_score = evaluator.evaluate(transformed_df)
-
-        #    assert math.fabs(pyspark_f1_score - spark_cuml_f1_score[0]) < 1e-6
