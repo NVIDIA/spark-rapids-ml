23723699c7ecac04e60417a92f40fd3501ae2ad0
Erik Ordentlich <eordentlich@gmail.com>
2023-07-07 17:54:34 -0700

    update notebook dataproc instructions and scripts
    Signed-off-by: Erik Ordentlich <eordentlich@gmail.com>

--- notebooks/dataproc/spark_rapids_ml.sh
+++ notebooks/dataproc/spark_rapids_ml.sh
@@ -5,6 +5,9 @@ RAPIDS_VERSION=23.6.0
 # patch existing packages
 mamba install "llvmlite<0.40,>=0.39.0dev0" "numba>=0.56.2"
 
+# dataproc 2.1 pyarrow and arrow conda installation is not compatible with cudf
+mamba uninstall -y pyarrow arrow
+
 # install cudf and cuml
 pip install --upgrade pip
 pip install cudf-cu11~=${RAPIDS_VERSION} cuml-cu11~=${RAPIDS_VERSION} \
