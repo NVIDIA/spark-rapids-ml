2f33d89c3b8fddbda9bf0932bf10f67e384ba0d9
Rishi <rishic@nvidia.com>
2023-08-17 11:10:56 -0700

    added umap to run_benchmark

--- python/run_benchmark.sh
+++ python/run_benchmark.sh
@@ -358,3 +358,29 @@ if [[ "${MODE}" =~ "random_forest_regressor" ]] || [[ "${MODE}" == "all" ]]; the
         $common_confs $spark_rapids_confs \
         ${EXTRA_ARGS}
 fi
+
+
+# UMAP
+if [[ "${MODE}" =~ "umap" ]] || [[ "${MODE}" == "all" ]]; then
+    if [[ ! -d "${gen_data_root}/blobs/r${num_rows}_c${num_cols}_float32.parquet" ]]; then
+        python $gen_data_script blobs \
+            --num_rows $num_rows \
+            --num_cols $num_cols \
+            --output_num_files $output_num_files \
+            --dtype "float32" \
+            --feature_type "array" \
+            --output_dir "${gen_data_root}/blobs/r${num_rows}_c${num_cols}_float32.parquet" \
+            $common_confs
+    fi
+
+    echo "$sep algo: umap $sep"
+    python ./benchmark/benchmark_runner.py umap \
+        --num_gpus $num_gpus \
+        --num_cpus $num_cpus \
+        --no_cache \
+        --num_runs $num_runs \
+        --train_path "${gen_data_root}/blobs/r${num_rows}_c${num_cols}_float32.parquet" \
+        --report_path "report_umap_${cluster_type}.csv" \
+        $common_confs $spark_rapids_confs \
+        ${EXTRA_ARGS}
+fi
