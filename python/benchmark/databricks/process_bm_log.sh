2218746cbde317088e3e759b5cd9e04806f3a7d9
Erik Ordentlich <eordentlich@gmail.com>
2023-08-17 09:52:16 -0700

    revert change to a script
    Signed-off-by: Erik Ordentlich <eordentlich@gmail.com>

--- python/benchmark/databricks/process_bm_log.sh
+++ python/benchmark/databricks/process_bm_log.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grep -oe '\(==* algo:.*==\|fit:........\|fit_time...........\|training::........\|parameters\|2023.*worker memory\|2023.*cuml fit\|2023.*fit complete\)' $1
+grep -oe '\(==* algo:.*==\|fit:........\|fit_time...........\|training::........\|parameters\)' $1
