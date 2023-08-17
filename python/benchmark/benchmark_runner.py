fd37f569b56b3d66129816172306f134c98db4c7
Rishi <rishic@nvidia.com>
2023-08-17 11:18:50 -0700

    umap in benchmark_runner

--- python/benchmark/benchmark_runner.py
+++ python/benchmark/benchmark_runner.py
@@ -24,6 +24,7 @@ from benchmark.bench_random_forest import (
     BenchmarkRandomForestClassifier,
     BenchmarkRandomForestRegressor,
 )
+from benchmark.bench_umap import BenchmarkUMAP
 
 
 class BenchmarkRunner:
@@ -35,6 +36,7 @@ class BenchmarkRunner:
             "pca": BenchmarkPCA,
             "random_forest_classifier": BenchmarkRandomForestClassifier,
             "random_forest_regressor": BenchmarkRandomForestRegressor,
+            "umap": BenchmarkUMAP,
         }
         algorithms = "\n    ".join(registered_algorithms.keys())
         parser = argparse.ArgumentParser(
