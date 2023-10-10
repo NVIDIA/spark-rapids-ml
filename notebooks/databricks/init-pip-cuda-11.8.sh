71194c88ea2bddef9f6b4081b0f46840a438a8fb
Erik Ordentlich <eordentlich@gmail.com>
2023-10-09 21:35:13 -0700

    retain pointers for azure databricks which still has old kernel driver
    Signed-off-by: Erik Ordentlich <eordentlich@gmail.com>

--- notebooks/databricks/init-pip-cuda-11.8.sh
+++ notebooks/databricks/init-pip-cuda-11.8.sh
@@ -13,6 +13,18 @@ curl -L https://repo1.maven.org/maven2/com/nvidia/rapids-4-spark_2.12/${SPARK_RA
 wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda_11.8.0_520.61.05_linux.run
 sh cuda_11.8.0_520.61.05_linux.run --silent --toolkit
 
+# install forward compatibility package due to old driver
+# uncomment below lines on Azure Databricks
+# distro=ubuntu2004
+# arch=x86_64
+# apt-key del 7fa2af80
+# wget https://developer.download.nvidia.com/compute/cuda/repos/$distro/$arch/cuda-keyring_1.0-1_all.deb
+# dpkg -i cuda-keyring_1.0-1_all.deb
+# apt-get update
+# apt-get install -y cuda-compat-11-8
+# export LD_LIBRARY_PATH=/usr/local/cuda/compat:/usr/local/cuda/lib64
+# ldconfig
+
 # reset symlink and update library loading paths
 rm /usr/local/cuda
 ln -s /usr/local/cuda-11.8 /usr/local/cuda
