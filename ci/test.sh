54dda50bceb90bece1020a85104d401a411638e5
YanxuanLiu <yanxuanl@nvidia.com>
2023-10-25 21:35:38 +0800

    uncomment test
    Signed-off-by: YanxuanLiu <yanxuanl@nvidia.com>

--- ci/test.sh
+++ ci/test.sh
@@ -39,17 +39,17 @@ cd python
 pip install -r requirements_dev.txt && pip install -e .
 
 # unit tests
-# ./run_test.sh $ut_args
+./run_test.sh $ut_args
 
 # benchmark
-# ./run_benchmark.sh $bench_args
+./run_benchmark.sh $bench_args
 
 # check compatibility with Spark 3.3 in nightly run
 # also push draft release docs to gh-pages
 if [[ $type == "nightly" ]]; then
     pip uninstall pyspark -y
     pip install pyspark~=3.3.0
-    # ./run_benchmark.sh $bench_args
+    ./run_benchmark.sh $bench_args
     # if everything passed till now update draft release docs in gh-pages
     # need to invoke docs.sh from top level of repo
     cd .. # top level of repo
