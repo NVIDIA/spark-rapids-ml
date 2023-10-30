3e5dceb3790d61b74a4b5f721d5bd7998663d985
eordentlich <eordentlich@gmail.com>
2023-10-30 10:46:02 -0700

    getActiveSession returned None in multiprocessing.Pool thread used in cv vs SparkSession.builder.getOrCreate(). (#488)
    Signed-off-by: Erik Ordentlich <eordentlich@gmail.com>

--- python/src/spark_rapids_ml/params.py
+++ python/src/spark_rapids_ml/params.py
@@ -27,6 +27,7 @@ from typing import (
     Union,
 )
 
+from pyspark import SparkContext
 from pyspark.ml.param import Param, Params, TypeConverters
 from pyspark.sql import SparkSession
 
@@ -355,7 +356,7 @@ class _CumlParams(_CumlClass, Params):
         """
         num_workers = 1
         try:
-            spark = SparkSession.getActiveSession()
+            spark = _get_spark_session()
             if spark:
                 sc = spark.sparkContext
                 if _is_local(sc):
