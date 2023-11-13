ce8557cfad245c05e16f05f93d26e4d9b15e0f53
Erik Ordentlich <eordentlich@gmail.com>
2023-11-13 13:57:25 -0800

    make tests compatible with spark 3.3 and add testing against 3.3. to nightly
    Signed-off-by: Erik Ordentlich <eordentlich@gmail.com>

--- ci/test.sh
+++ ci/test.sh
@@ -49,6 +49,7 @@ pip install -r requirements_dev.txt && pip install -e .
 if [[ $type == "nightly" ]]; then
     pip uninstall pyspark -y
     pip install pyspark~=3.3.0
+    ./run_test.sh $ut_args
     ./run_benchmark.sh $bench_args
     # if everything passed till now update draft release docs in gh-pages
     # need to invoke docs.sh from top level of repo
