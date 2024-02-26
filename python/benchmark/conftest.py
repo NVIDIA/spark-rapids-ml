892e7a315b49fd0df0547ebf708c81227b8cbfee
Hongzhe Cheng <hongzhec@Nvidia.com>
2024-02-26 14:16:19 -0800

    Conftest import for slow testcases

--- python/benchmark/conftest.py
+++ python/benchmark/conftest.py
@@ -14,28 +14,8 @@
 # limitations under the License.
 #
 
-from typing import List
+import sys
+import os
 
-import pytest
-
-
-def pytest_addoption(parser: pytest.Parser) -> None:
-    parser.addoption(
-        "--runslow", action="store_true", default=False, help="run slow tests"
-    )
-
-
-def pytest_configure(config: pytest.Config) -> None:
-    config.addinivalue_line("markers", "slow: mark as slow to run")
-
-
-def pytest_collection_modifyitems(
-    config: pytest.Config, items: List[pytest.Item]
-) -> None:
-    if config.getoption("--runslow"):
-        # --runslow given in cli: do not skip slow tests
-        return
-    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
-    for item in items:
-        if "slow" in item.keywords:
-            item.add_marker(skip_slow)
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from tests.conftest import pytest_addoption, pytest_configure, pytest_collection_modifyitems
