1ef3ca8c0a49dabdbccf1e113e46c28d553f216a
spark-rapids automation <70000568+nvauto@users.noreply.github.com>
2024-03-21 23:41:13 +0000

    Update draft api docs to commit v24.02.0 on

--- api/python/_static/documentation_options.js
+++ api/python/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '23.12.0',
+    VERSION: '24.02.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
