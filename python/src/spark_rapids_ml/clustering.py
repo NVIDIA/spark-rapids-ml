DBSCAN broadcast fix (#617)
* DBSCAN broadcast fix

Signed-off-by: Hongzhe Cheng <hongzhec@nvidia.com>

* Comment delete

---------

Signed-off-by: Hongzhe Cheng <hongzhec@nvidia.com>

python/src/spark_rapids_ml/clustering.py
@@ -914,25 +914,8 @@ class DBSCANModel(
             == "true"
         )
 
-        inputs = []  # type: ignore
-
-        idCol = list(
-            self.idCols_[0].value
-            if len(self.idCols_) == 1
-            else np.concatenate([chunk.value for chunk in self.idCols_])
-        )
-
-        for pdf_bc in self.raw_data_:
-            features = pdf_bc.value
-
-            # experiments indicate it is faster to convert to numpy array and then to cupy array than directly
-            # invoking cupy array on the list
-            if cuda_managed_mem_enabled:
-                features = cp.array(features)
-
-            inputs.append(features)
-
-        concated = _concat_and_free(inputs, order=array_order)
+        idCol_bc = self.idCols_
+        raw_data_dc = self.raw_data_
 
         def _cuml_fit(
             dfs: FitInputType,
@@ -941,10 +924,28 @@ class DBSCANModel(
             from cuml.cluster.dbscan_mg import DBSCANMG as CumlDBSCANMG
             from pyspark import BarrierTaskContext
 
+            inputs = []  # type: ignore
+
+            idCol = list(
+                idCol_bc[0].value
+                if len(idCol_bc) == 1
+                else np.concatenate([chunk.value for chunk in idCol_bc])
+            )
+
+            for pdf_bc in raw_data_dc:
+                features = pdf_bc.value
+
+                # experiments indicate it is faster to convert to numpy array and then to cupy array than directly
+                # invoking cupy array on the list
+                if cuda_managed_mem_enabled:
+                    features = cp.array(features)
+
+                inputs.append(features)
+
+            concated = _concat_and_free(inputs, order=array_order)
+
             context = BarrierTaskContext.get()
             partition_id = context.partitionId()
-
-            logger = get_logger(self.__class__)
 
             dbscan = CumlDBSCANMG(
                 handle=params[param_alias.handle],