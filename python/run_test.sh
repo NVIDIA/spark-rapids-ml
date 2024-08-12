28fe4f8abd78c5c9f18c9d73159182af27a3c355
Jinfeng <jinfengl@nvidia.com>
2024-08-12 20:59:50 +0000

    reset run_test.sh to normal

--- python/run_test.sh
+++ python/run_test.sh
@@ -9,8 +9,6 @@ then
     echo "Tests use at most 4 GPUs. If failed, try setting CUDA_VISIBLE_DEVICES."
 fi
 echo "use --runslow to run all tests"
-#pytest "$@" benchmark/test_gen_data.py
-PYTHONPATH=`pwd`/benchmark pytest -s --runslow tests/test_logistic_regression.py::test_standardization_sparse_example
-PYTHONPATH=`pwd`/benchmark pytest -s --runslow tests/test_logistic_regression.py::test_sparse_int64
-#PYTHONPATH=`pwd`/benchmark pytest -ra "$@" --durations=10 tests
+pytest "$@" benchmark/test_gen_data.py
+PYTHONPATH=`pwd`/benchmark pytest -ra "$@" --durations=10 tests
 #PYTHONPATH=`pwd`/benchmark pytest -ra --runslow --durations=10 tests
