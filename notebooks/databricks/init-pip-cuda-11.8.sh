36a09791ed9aecfcbccb8f6a655ff254b85c7a20
eordentlich <eordentlich@gmail.com>
2024-08-25 15:43:20 -0700

    changes to support db 13.3+ (#716)
    * changes to support db 13.3+
    
    Signed-off-by: Erik Ordentlich <eordentlich@gmail.com>
    
    * cleanup and add 15.4 as db version option
    
    Signed-off-by: Erik Ordentlich <eordentlich@gmail.com>
    
    * address comments
    
    Signed-off-by: Erik Ordentlich <eordentlich@gmail.com>
    
    ---------
    
    Signed-off-by: Erik Ordentlich <eordentlich@gmail.com>

--- notebooks/databricks/init-pip-cuda-11.8.sh
+++ notebooks/databricks/init-pip-cuda-11.8.sh
@@ -5,7 +5,7 @@ SPARK_RAPIDS_ML_ZIP=/dbfs/path/to/zip/file
 # also in general, RAPIDS_VERSION (python) fields should omit any leading 0 in month/minor field (i.e. 23.8.0 and not 23.08.0)
 # while SPARK_RAPIDS_VERSION (jar) should have leading 0 in month/minor (e.g. 23.08.2 and not 23.8.2)
 RAPIDS_VERSION=24.8.0
-SPARK_RAPIDS_VERSION=24.04.1
+SPARK_RAPIDS_VERSION=24.06.1
 
 curl -L https://repo1.maven.org/maven2/com/nvidia/rapids-4-spark_2.12/${SPARK_RAPIDS_VERSION}/rapids-4-spark_2.12-${SPARK_RAPIDS_VERSION}-cuda11.jar -o /databricks/jars/rapids-4-spark_2.12-${SPARK_RAPIDS_VERSION}.jar
 
