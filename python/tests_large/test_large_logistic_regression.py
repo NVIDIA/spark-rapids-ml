dedbf8193365f34000ee5ea1fc098b9501bc6e75
Jinfeng Li <jinfengl@nvidia.com>
2024-09-03 22:44:02 +0000

    add a test case to test int64 with multi GPUs
    Signed-off-by: Jinfeng <jinfengl@nvidia.com>

--- python/tests_large/test_large_logistic_regression.py
+++ python/tests_large/test_large_logistic_regression.py
@@ -13,12 +13,12 @@ from tests.test_logistic_regression import compare_model
 from .conftest import _spark
 
 
-def test_sparse_int64() -> None:
+def test_sparse_int64(multi_gpus: bool = False) -> None:
     """
     This test requires minimum 128G CPU memory, 32 GB GPU memory
     TODO: move generated dataset to a unified place
     """
-    gpu_number = 1
+    gpu_number = 2 if multi_gpus else 1
     output_num_files = 100  # large value smaller CPU memory for each spark task
     data_shape = (int(1e7), 2200)
 
@@ -63,7 +63,24 @@ def test_sparse_int64() -> None:
 
     data_gen = SparseRegressionDataGen(data_gen_args)
     df, _, _ = data_gen.gen_dataframe_and_meta(_spark)
+
     df = df.cache()
+    df_gpu = df
+
+    if gpu_number > 1:
+        main_pid = 0
+        pid_col = "pid"
+        delta_ratio = 0.1
+
+        delta_df = df.sample(fraction=delta_ratio, seed=0)
+
+        df = df.withColumn(pid_col, SparkF.lit(main_pid))
+        delta_df = delta_df.withColumn(
+            pid_col, SparkF.monotonically_increasing_id() % (gpu_number * 4)
+        )
+
+        df = df.union(delta_df)
+        df_gpu = df.repartition(gpu_number, pid_col)
 
     def get_nnz_func(pdf_iter: Iterable[pd.DataFrame]) -> Iterable[pd.DataFrame]:
         for pdf in pdf_iter:
@@ -77,7 +94,7 @@ def test_sparse_int64() -> None:
 
     # compare gpu with spark cpu
     gpu_est = LogisticRegression(num_workers=gpu_number, verbose=True, **est_params)
-    gpu_model = gpu_est.fit(df)
+    gpu_model = gpu_est.fit(df_gpu)
     cpu_est = SparkLogisticRegression(**est_params)
     cpu_model = cpu_est.fit(df)
     cpu_objective = cpu_model.summary.objectiveHistory[-1]
@@ -95,3 +112,10 @@ def test_sparse_int64() -> None:
         total_tol=tolerance,
         accuracy_and_probability_only=True,
     )
+
+    if gpu_number == 1:
+        assert gpu_est._index_dtype == "int64"
+
+
+def test_sparse_int64_mg() -> None:
+    test_sparse_int64(multi_gpus=True)
