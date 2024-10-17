f65869e21361e43c5a2a940fe2f8a044c770a658
Jinfeng <jinfengl@nvidia.com>
2024-10-17 22:56:50 +0000

    revise to use minimum GPU memory to determine parallelism handle corner case minimal 1 pytest worker get CUDA_VISIBLE_DEVICES working

--- python/run_test.sh
+++ python/run_test.sh
@@ -20,7 +20,11 @@ spark-rapids-submit --master local[1] tests_no_import_change/test_no_import_chan
 spark-submit --master local[1] tests_no_import_change/test_no_import_change.py 0.2
 
 # calculate pytest parallelism by following https://github.com/NVIDIA/spark-rapids/blob/branch-24.12/integration_tests/run_pyspark_from_build.sh
-GPU_MEM_PARALLEL=`nvidia-smi --query-gpu=memory.free --format=csv,noheader | awk 'NR == 1 { MIN = $1 } { if ($1 < MIN) { MIN = $1 } } END { print int((MIN - 2 * 1024) / ((1.5 * 1024) + 750)) }'`
+NVIDIA_SMI_ARGS="" 
+if [ ${CUDA_VISIBLE_DEVICES} ]; then
+    NVIDIA_SMI_ARGS="${NVIDIA_SMI_ARGS} -i ${CUDA_VISIBLE_DEVICES}" 
+fi
+GPU_MEM_PARALLEL=`nvidia-smi ${NVIDIA_SMI_ARGS} --query-gpu=memory.free --format=csv,noheader | awk 'NR == 1 { MIN = $1 } { if ($1 < MIN) { MIN = $1 } } END { print int((MIN - 2 * 1024) / ((1.5 * 1024) + 750)) }'`
 CPU_CORES=`nproc`
 HOST_MEM_PARALLEL=`cat /proc/meminfo | grep MemAvailable | awk '{print int($2 / (8 * 1024 * 1024))}'`
 TMP_PARALLEL=$(( $GPU_MEM_PARALLEL > $CPU_CORES ? $CPU_CORES : $GPU_MEM_PARALLEL ))
