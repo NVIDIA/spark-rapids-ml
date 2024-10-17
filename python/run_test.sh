a3750ed12fcb0b1318f6475990792d36dcb3a4c3
Jinfeng <jinfengl@nvidia.com>
2024-10-17 22:19:53 +0000

    revise from using max gpu memory to minimum gpu memory

--- python/run_test.sh
+++ python/run_test.sh
@@ -19,14 +19,19 @@ spark-rapids-submit --master local[1] tests_no_import_change/test_no_import_chan
 # runs on cpu with spark-submit
 spark-submit --master local[1] tests_no_import_change/test_no_import_change.py 0.2
 
-# calculate pytest parallelism following https://github.com/NVIDIA/spark-rapids/blob/branch-24.12/integration_tests/run_pyspark_from_build.sh
-GPU_MEM_PARALLEL=`nvidia-smi --query-gpu=memory.free --format=csv,noheader | awk '{if (MAX < $1){ MAX = $1}} END {print int((MAX - 2 * 1024) / ((1.5 * 1024) + 750))}'`
+# calculate pytest parallelism by following https://github.com/NVIDIA/spark-rapids/blob/branch-24.12/integration_tests/run_pyspark_from_build.sh
+GPU_MEM_PARALLEL=`nvidia-smi --query-gpu=memory.free --format=csv,noheader | awk 'NR == 1 { MIN = $1 } { if ($1 < MIN) { MIN = $1 } } END { print int((MIN - 2 * 1024) / ((1.5 * 1024) + 750)) }'`
 CPU_CORES=`nproc`
 HOST_MEM_PARALLEL=`cat /proc/meminfo | grep MemAvailable | awk '{print int($2 / (8 * 1024 * 1024))}'`
 TMP_PARALLEL=$(( $GPU_MEM_PARALLEL > $CPU_CORES ? $CPU_CORES : $GPU_MEM_PARALLEL ))
 TMP_PARALLEL=$(( $TMP_PARALLEL > $HOST_MEM_PARALLEL ? $HOST_MEM_PARALLEL : $TMP_PARALLEL ))
 TEST_PARALLEL=${TMP_PARALLEL}
-echo "${TEST_PARALLEL} pytest workers will be used to execute test functions in benchmark/test_gen_data.py and tests/ directory in parallel"
+if  (( $TMP_PARALLEL <= 1 )); then
+    TEST_PARALLEL=1
+else
+TEST_PARALLEL=$TMP_PARALLEL
+fi
+echo "Test functions in benchmark/test_gen_data.py and tests/ directory will be executed in parallel with ${TEST_PARALLEL} pytest workers"
 
 echo "use --runslow to run all tests"
 pytest "$@" -n ${TEST_PARALLEL} benchmark/test_gen_data.py
