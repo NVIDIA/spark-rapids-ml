55f073c716cd998fbd831612601c1bea42a2841c
Bobby Wang <wbo4958@gmail.com>
2025-02-20 10:31:41 +0800

    fix
    Signed-off-by: Bobby Wang <wbo4958@gmail.com>

--- python/src/spark_rapids_ml/connect_plugin.py
+++ python/src/spark_rapids_ml/connect_plugin.py
@@ -1,4 +1,6 @@
 #
+# Copyright (c) 2025, NVIDIA CORPORATION.
+#
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
 # this work for additional information regarding copyright ownership.
