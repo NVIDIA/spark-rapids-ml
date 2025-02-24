0a7f2498237c63380a6d24b2ee928c4587164bed
eordentlich <eordentlich@gmail.com>
2025-02-24 09:34:05 -0800

    update rapids to official release 25.02 (#850)
    Signed-off-by: Erik Ordentlich <eordentlich@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2024, NVIDIA CORPORATION.
+# Copyright (c) 2025, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -23,7 +23,7 @@
 project = 'spark-rapids-ml'
 copyright = '2024, NVIDIA'
 author = 'NVIDIA'
-release = '24.12.0'
+release = '25.02.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
