6210e37ef0d24847db868f6a098f84f46ca9098d
jenkins <jenkins@localhost>
2025-03-14 08:12:51 +0000

    Update draft api docs to commit v25.02 on

--- api/python/_static/documentation_options.js
+++ api/python/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '24.12.0',
+    VERSION: '25.02.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
