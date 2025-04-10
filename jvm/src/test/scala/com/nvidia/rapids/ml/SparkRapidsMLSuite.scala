74b9014a2c00027d40e7c10655bfce8f227015f2
Bobby Wang <bobwang@nvidia.com>
2025-04-10 08:13:16 +0800

    Add unit test for connect ml plugin (#883)
    The added unit tests are not testing the end-to-end case for the
    'connect'. Instead, the tests simply ensure that the functionality of
    the spark rapids ml JVM estimator/model could work even without
    'connect' , behaving like a normal ML operator.
    
    ---------
    
    Signed-off-by: Bobby Wang <wbo4958@gmail.com>

---
+++ jvm/src/test/scala/com/nvidia/rapids/ml/SparkRapidsMLSuite.scala
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2025, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.nvidia.rapids.ml
+
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.funsuite.AnyFunSuite
+
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.sql.SparkSession
+
+class SparkRapidsMLSuite extends AnyFunSuite with BeforeAndAfterEach {
+  @transient var ss: SparkSession = _
+
+  override def beforeEach(): Unit = {
+    try {
+      ss = SparkSession.builder()
+        .master("local[1]")
+        // TODO add spark-rapids after spark-rapids has supported 4.0
+//        .config("spark.sql.adaptive.enabled", "false")
+//        .config("spark.stage.maxConsecutiveAttempts", "1")
+//        .config("spark.plugins", "com.nvidia.spark.SQLPlugin")
+//        .config("spark.rapids.memory.gpu.pool", "none") // Disable RMM for unit tests.
+        .appName("SparkRapidsML-connect-plugin")
+        .getOrCreate()
+    } finally {
+      super.beforeEach()
+    }
+  }
+
+  override def afterEach(): Unit = {
+    try {
+      if (ss != null) {
+        ss.stop()
+        ss = null
+      }
+    } finally {
+      super.afterEach()
+    }
+  }
+
+  test("RapidsLogisticRegression") {
+    val df = ss.createDataFrame(
+      Seq(
+        (Vectors.dense(1.0, 2.0), 1.0f),
+        (Vectors.dense(1.0, 3.0), 1.0f),
+        (Vectors.dense(2.0, 1.0), 0.0f),
+        (Vectors.dense(3.0, 1.0), 0.0f))
+    ).toDF("test_feature", "class")
+
+    val lr = new RapidsLogisticRegression()
+      .setFeaturesCol("test_feature")
+      .setLabelCol("class")
+      .setMaxIter(23)
+      .setTol(0.03)
+    // .setThreshold(0.51)  // this is going to fail due to spark-rapids-ml has mapped threshold to None
+
+    val model = lr.fit(df)
+
+    assert(model.getFeaturesCol == "test_feature")
+    assert(model.getTol == 0.03)
+    assert(model.getLabelCol == "class")
+    assert(model.getMaxIter == 23)
+  }
+}
