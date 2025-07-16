a3793804221596b1774cf30ab7225572a872aebf
YanxuanLiu <yanxuanl@nvidia.com>
2025-07-16 10:38:15 +0800

    drop cuda11 support (#959)
    fix https://github.com/NVIDIA/spark-rapids-ml/issues/957
    
    - Updated default cuda version to 12.9.1
    - TODO: `notebooks/databricks/init-pip-cuda-11.8.sh`
    
    ---------
    
    Signed-off-by: Yanxuan Liu <yanxuanl@nvidia.com>

--- python/benchmark/databricks/init-pip-cuda-12.0.sh
+++ python/benchmark/databricks/init-pip-cuda-12.0.sh
@@ -13,43 +13,40 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -ex
-
-# IMPORTANT: specify RAPIDS_VERSION fully 23.10.0 and not 23.10
-# also in general, RAPIDS_VERSION (python) fields should omit any leading 0 in month/minor field (i.e. 23.8.0 and not 23.08.0)
+# set portion of path below after /dbfs/ to dbfs zip file location
+SPARK_RAPIDS_ML_ZIP=/dbfs/path/to/spark-rapids-ml.zip
+BENCHMARK_ZIP=/dbfs/path/to/benchmark.zip
+# IMPORTANT: specify rapids fully 23.10.0 and not 23.10
+# also, in general, RAPIDS_VERSION (python) fields should omit any leading 0 in month/minor field (i.e. 23.8.0 and not 23.08.0)
 # while SPARK_RAPIDS_VERSION (jar) should have leading 0 in month/minor (e.g. 23.08.2 and not 23.8.2)
 RAPIDS_VERSION=25.6.0
 SPARK_RAPIDS_VERSION=25.04.0
 
 curl -L https://repo1.maven.org/maven2/com/nvidia/rapids-4-spark_2.12/${SPARK_RAPIDS_VERSION}/rapids-4-spark_2.12-${SPARK_RAPIDS_VERSION}-cuda11.jar -o /databricks/jars/rapids-4-spark_2.12-${SPARK_RAPIDS_VERSION}.jar
 
-# install cudatoolkit 11.8 via runfile approach
-wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda_11.8.0_520.61.05_linux.run
-sh cuda_11.8.0_520.61.05_linux.run --silent --toolkit
+# install cudatoolkit 12.0 via runfile approach
+wget https://developer.download.nvidia.com/compute/cuda/12.0.1/local_installers/cuda_12.0.1_525.85.12_linux.run
+sh cuda_12.0.1_525.85.12_linux.run --silent --toolkit
 
 # reset symlink and update library loading paths
+# **** set LD_LIBRARY_PATH as below in env var section of cluster config in DB cluster UI ****
 rm /usr/local/cuda
-ln -s /usr/local/cuda-11.8 /usr/local/cuda
+ln -s /usr/local/cuda-12.0 /usr/local/cuda
 
 # upgrade pip
 /databricks/python/bin/pip install --upgrade pip
 
-# install cudf, cuml and their rapids dependencies
-# using ~= pulls in latest micro version patches
+# install cudf and cuml
+# using ~= pulls in micro version patches
 /databricks/python/bin/pip install cudf-cu11~=${RAPIDS_VERSION} \
     cuml-cu11~=${RAPIDS_VERSION} \
     cuvs-cu11~=${RAPIDS_VERSION} \
+    pylibraft-cu11~=${RAPIDS_VERSION} \
+    rmm-cu11~=${RAPIDS_VERSION} \
     --extra-index-url=https://pypi.nvidia.com
 
 # install spark-rapids-ml
-/databricks/python/bin/pip install spark-rapids-ml
-
-# set up no-import-change for cluster if enabled
-if [[ $SPARK_RAPIDS_ML_NO_IMPORT_ENABLED == 1 ]]; then
-    echo "enabling no import change in cluster" 1>&2
-    mkdir -p /root/.ipython/profile_default/startup
-    echo "import spark_rapids_ml.install" >/root/.ipython/profile_default/startup/00-spark-rapids-ml.py
-fi
-
-
+python_ver=`python --version | grep -oP '3\.[0-9]+'`
+unzip ${SPARK_RAPIDS_ML_ZIP} -d /databricks/python3/lib/python${python_ver}/site-packages
+unzip ${BENCHMARK_ZIP} -d /databricks/python3/lib/python${python_ver}/site-packages
 
