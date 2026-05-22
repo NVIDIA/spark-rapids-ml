9e5cab04faeb46fa03281d81a9ab0ef76c5b43eb
Murray Todd Williams <murray.williams@lovelytics.com>
2026-05-22 12:50:24 -0500

    Reverting back to 26.4.2 and 26.4.0
    Signed-off-by: Murray Todd Williams <murray.williams@lovelytics.com>

--- notebooks/databricks/init-pip-cuda-12.sh
+++ notebooks/databricks/init-pip-cuda-12.sh
@@ -21,8 +21,8 @@ set -ex
 # 
 # Note also that sometimes the jar and python packages will have different patch versions published and available at any time,
 # so the versions may not perfectly align. This is expected and should not cause issues.
-RAPIDS_VERSION=25.12.0
-SPARK_RAPIDS_VERSION=25.12.0
+RAPIDS_VERSION=26.4.0
+SPARK_RAPIDS_VERSION=26.04.2
 
 curl -L https://repo1.maven.org/maven2/com/nvidia/rapids-4-spark_2.13/${SPARK_RAPIDS_VERSION}/rapids-4-spark_2.13-${SPARK_RAPIDS_VERSION}-cuda12.jar -o /databricks/jars/rapids-4-spark_2.13-${SPARK_RAPIDS_VERSION}.jar
 
